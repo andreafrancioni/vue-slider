451004f5d1074c5e03cd4ad4d1c68f7477af44ab
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-16 15:58:38 +0100

    Inseriti array di oggetti in pagina

--- js/main.js
+++ js/main.js
@@ -4,7 +4,24 @@ const { createApp } = Vue;
 const opzioni = {
     data: function () {
         return {
-            message: 'Benvenuto',
+            slides: [
+                {
+                    immagine: 'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg',
+                    titolo: 'Tramonto sulla spiaggia',
+                    descrizione: 'Un meraviglioso tramonto con colori caldi che si riflettono sull\'acqua.'
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees-covered-snow-fog_181624-17590.jpg',
+                    titolo: 'Montagne innevate',
+                    descrizione: 'Un panorama mozzafiato delle montagne coperte di neve.'
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-photo/countryside-field-sunny-day-countryside_181624-24010.jpg',
+                    titolo: 'Campi verdi',
+                    descrizione: 'Vaste distese di campi verdi che si estendono all\'orizzonte.'
+                }
+            ],
+            slideNumber: 0,
 
         }
     }
